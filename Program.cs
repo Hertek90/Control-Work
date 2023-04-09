a4479edee024d9c3ed9d1a4cba6be4e8b9a5ef7f
«Apolon» <hertek1990@gmail.com>
2023-04-09 21:56:04 +0700

    Stage 3

--- Program.cs
+++ Program.cs
@@ -11,3 +11,20 @@
 Console.WriteLine("Введите строки массива через пробел: ");
 string[] array1 = Console.ReadLine()!.Split(' ');
 string[] array2 = new string[array1.Length];
+int count = 0;
+for(int i = 0; i < array1.Length; i++)
+{
+    if(array1[i].Length <= 3)
+    {
+        array2[count] = array1[i];
+        count++;
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
